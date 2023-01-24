21c61c0be1d40631ed9fe81cfa5598d048fe852c
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-24 14:22:45 +0330

    Add files via upload

---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = 0;
+            this.Top = 0;
+            this.Width = 0;
+            this.Height = 0;
+            timer1.Interval = 1;
+            timer2.Interval = 1;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Width += 10;
+            if(this.Width>700)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Height += 10;
+            if (this.Height > 700)
+                timer2.Stop();
+        }
+    }
+}
